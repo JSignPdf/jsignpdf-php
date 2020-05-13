6b17e194046041693cd5dc06e639791cdf657a58
jeidison <jeidison.farias@gmail.com>
2020-05-13 11:06:34 -0300

    example

--- example/index.php
+++ example/index.php
@@ -6,11 +6,11 @@ use Jeidison\JSignPDF\JSignPDF;
 
 try {
     $jSignPdf = new JSignPDF();
-    $jSignPdf->setCertificate(file_get_contents("../tests/resources/teste.pfx"));
-    $jSignPdf->setPassword('lorena131');
+    $jSignPdf->setCertificate(file_get_contents("../tests/resources/test.pfx"));
+    $jSignPdf->setPassword('test');
     $jSignPdf->setPdf(file_get_contents('../tests/resources/pdf-test.pdf'));
     $fileSigned = $jSignPdf->sign()->output();
-    file_put_contents('../tmp/teste.pdf', $fileSigned);
+    file_put_contents('../tmp/test.pdf', $fileSigned);
 } catch (Exception $e) {
     var_dump($e);
 }
